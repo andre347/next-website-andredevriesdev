ac19f630953ef14727b44734fab8c8c5952e940d
andre347 <aedevries19@gmail.com>
2020-08-16 09:07:00 +0100

    FEAT: Setup Purge in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: [],
+  purge: ["./src/**/*.html", "./src/**/*.html"],
   theme: {
     extend: {
       fontFamily: {
