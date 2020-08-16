e70a66e26872242e964cf5e82aa5b5781500eb95
andre347 <aedevries19@gmail.com>
2020-08-16 09:27:34 +0100

    Revert Purge

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,15 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  // Specify the paths to all of the template files in your project
-  content: [
-    "./pages/*.js",
-    "./pages/**/*.js",
-    "./components/*.js",
-    "./components/**/*.js",
-  ],
-
-  // make sure css reset isnt removed on html and body
-  whitelist: ["html", "body"],
-
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
-
 module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === "production"
+      ? {
+          "@fullhuman/postcss-purgecss": {
+            content: ["./components/**/*.js", "./pages/**/*.js"],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          },
+        }
+      : {}),
+  },
 };
