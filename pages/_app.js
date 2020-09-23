16ef4ab02fc8050b62d7b62cba87583e7f5411d0
andre347 <aedevries19@gmail.com>
2020-09-23 17:49:28 +0100

    Update to Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import React, { Fragment } from "react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
 import "../css/tailwind.css";
 import Head from "next/head";
@@ -25,6 +28,16 @@ const meta = {
 };
 
 function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <div className="antialiased">
       <Head>
--- pages/_document.js
+++ pages/_document.js
@@ -1,58 +1,35 @@
-import { Fragment } from "react";
-import Document, { Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 import { GA_TRACKING_ID } from "../lib/gtag";
 
-export default class CustomDocument extends Document {
-  static async getInitialProps(ctx) {
-    const originalRenderPage = ctx.renderPage;
-
-    const initialProps = await Document.getInitialProps(ctx);
-
-    // Check if in production
-    const isProduction = process.env.NODE_ENV === "production";
-
-    return {
-      ...initialProps,
-      isProduction,
-    };
-  }
-
+export default class MyDocument extends Document {
   render() {
-    const { isProduction } = this.props;
-
     return (
-      <html lang="en">
+      <Html lang="en">
         <Head>
-          {/* We only want to add the scripts if in production */}
-          {isProduction && (
-            <Fragment>
-              {/* Global Site Tag (gtag.js) - Google Analytics */}
-              <script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-              />
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-
-                    gtag('config', '${GA_TRACKING_ID}', {
-                      page_path: window.location.pathname,
-                    });
-                  `,
-                }}
-              />
-            </Fragment>
-          )}
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
