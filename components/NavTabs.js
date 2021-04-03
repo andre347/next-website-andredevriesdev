7ba700be21a4cd2f2d45c685a2ed8fe949e0a0ac
andre347 <aedevries19@gmail.com>
2021-04-03 17:58:39 +0100

    This works?

--- components/NavTabs.js
+++ components/NavTabs.js
@@ -1,5 +1,7 @@
 import React from "react";
 import NavLink from "@/lib/NavLink";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const navItems = [
   { url: "/", id: "Home" },
@@ -9,7 +11,24 @@ const navItems = [
   { url: "/pages/youtube", id: "YouTube" },
 ];
 
+const routes = {
+  home: {
+    label: "Home",
+    path: "/",
+  },
+  about: {
+    label: "About",
+    path: "/pages/about",
+  },
+};
+
+const defaultRoutes = [routes.home, routes.about];
+
 function NavTabs() {
+  const router = useRouter();
+  const currPathName = router.pathname;
+  const routesAsArr = Object.keys(routes).map((r) => routes[r]);
+
   return (
     // <div className="py-4 mt-2">
     <div className="hidden sm:block">
@@ -17,12 +36,31 @@ function NavTabs() {
         <nav className="flex" aria-label="Tabs">
           {navItems.map((item, idx) => {
             return (
-              <NavLink href={item.url} key={idx}>
+              <NavLink href={item.url} key={item.url}>
                 <a aria-current="page">{item.id}</a>
               </NavLink>
             );
           })}
         </nav>
+        {/* <div className="hidden max-w-screen-md grid-cols-4 gap-1 mx-auto md:grid">
+          {defaultRoutes.map((route) => {
+            const isActive = route.path === router.pathname;
+            const defaultClasses = `font-sans font-semibold flex rounded items-center text-opacity-40 justify-center py-2 text-sm`;
+            const activeClasses = `bg-gray-200`;
+            const inactiveClasses = `hover:bg-gray-900 filter-saturate hover:bg-opacity-5 dark:hover:bg-white dark:text-white  hover:text-gray-200 dark:hover:text-gray-100 text-tertiary`;
+            return (
+              <Link href={route.path} key={route.path}>
+                <a
+                  className={`
+                ${defaultClasses} 
+                ${isActive ? activeClasses : inactiveClasses}`}
+                >
+                  {route.label}
+                </a>
+              </Link>
+            );
+          })}
+        </div> */}
       </div>
     </div>
     // </div>
