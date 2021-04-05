570ce7dec52ccba981ecb123120718c1ba8dc8db
andre347 <aedevries19@gmail.com>
2021-04-05 13:21:14 +0100

    Added cache control for youtube api route

--- pages/api/youtube.js
+++ pages/api/youtube.js
@@ -1,6 +1,32 @@
 import { google } from "googleapis";
 import xml2js from "xml2js";
 
+async function getLatestVideos() {
+  const response = await fetch(
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UC6oFbYx6YjE596P-Ty8bVyg"
+  );
+  const parsedResponse = await response.text();
+  const result = await xml2js.parseStringPromise(parsedResponse, {
+    mergeAttrs: true,
+  });
+  const entry = result.feed.entry.slice(0, 4);
+  const mappedEntries = entry.map((video) => {
+    return {
+      id: video.id[0],
+      title: video.title[0],
+      published: video.published[0],
+      thumbnail: video["media:group"][0]["media:thumbnail"][0]["url"][0],
+      description: video["media:group"][0]["media:description"][0],
+      views:
+        video["media:group"][0]["media:community"][0]["media:statistics"][0][
+          "views"
+        ][0],
+    };
+  });
+  // convert it to a JSON string
+  return mappedEntries;
+}
+
 export default async (_, res) => {
   const auth = new google.auth.GoogleAuth({
     credentials: {
@@ -16,39 +42,6 @@ export default async (_, res) => {
     version: "v3",
   });
 
-  const videoResponse = await youtube.search.list({
-    part: ["snippet"],
-    forMine: true,
-    maxResults: 5,
-    type: ["video"],
-  });
-
-  async function getLatestVideos() {
-    const response = await fetch(
-      "https://www.youtube.com/feeds/videos.xml?channel_id=UC6oFbYx6YjE596P-Ty8bVyg"
-    );
-    const parsedResponse = await response.text();
-    const result = await xml2js.parseStringPromise(parsedResponse, {
-      mergeAttrs: true,
-    });
-    const entry = result.feed.entry.slice(0, 4);
-    const mappedEntries = entry.map((video) => {
-      return {
-        id: video.id[0],
-        title: video.title[0],
-        published: video.published[0],
-        thumbnail: video["media:group"][0]["media:thumbnail"][0]["url"][0],
-        description: video["media:group"][0]["media:description"][0],
-        views:
-          video["media:group"][0]["media:community"][0]["media:statistics"][0][
-            "views"
-          ][0],
-      };
-    });
-    // convert it to a JSON string
-    return mappedEntries;
-  }
-
   const response = await youtube.channels.list({
     id: "UC6oFbYx6YjE596P-Ty8bVyg",
     part: "statistics",
@@ -57,6 +50,9 @@ export default async (_, res) => {
   const channel = response.data.items[0];
   const { subscriberCount, viewCount, videoCount } = channel.statistics;
   const videoStats = await getLatestVideos();
+
+  // set the cache control to max cache for 5 minutes
+  res.setHeader("Cache-Control", "s-maxage=300000, stale-while-revalidate");
 
   return res.status(200).json({
     subscriberCount,
