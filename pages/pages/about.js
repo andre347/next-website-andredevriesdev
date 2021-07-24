ab431e39b90405a21b726b9ee7d45b95d76285a8
Andre <andre.devries@theinformationlab.co.uk>
2021-07-24 20:28:28 +0200

    New blog post: using Google Sheet as a CMS in Nextjs

--- pages/pages/about.js
+++ pages/pages/about.js
@@ -60,6 +60,18 @@ function About() {
             </a>{" "}
             page.
           </p>
+          {/* <figure>
+            <img
+              className="w-full rounded-lg"
+              src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3"
+              alt=""
+              width={1310}
+              height={873}
+            />
+            <figcaption>
+              Sagittis scelerisque nulla cursus in enim consectetur quam.
+            </figcaption>
+          </figure> */}
           <h3>Teaching</h3>
           <p>
             During my career I have helped hundreds of companies, many of those
