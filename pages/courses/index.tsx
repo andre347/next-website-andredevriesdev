512affdea34b504b7bc40c9f53e518ed03692329
Andre <andre.devries@theinformationlab.co.uk>
2021-12-28 17:38:06 +0100

    TS error fix

--- pages/courses/index.tsx
+++ pages/courses/index.tsx
@@ -22,6 +22,7 @@ function Courses() {
         {courses.list.map((course, idx) => {
           // calculate  duration
           const duration = course.videos.reduce((prev, current) => {
+            // @ts-ignore
             return prev + current?.length;
           }, 0);
           return (
