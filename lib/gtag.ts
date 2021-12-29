134a38a003f555cd912e29e51eac939312dc2052
Andre <andre.devries@theinformationlab.co.uk>
2021-12-29 09:23:40 +0100

    TS error on Gtag

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -3,16 +3,18 @@ const { NODE_ENV } = process.env;
 export const GA_TRACKING_ID =
   NODE_ENV !== "development" ? "UA-90804640-2" : null; // This is your GA Tracking ID
 
+(window as any).MyNamespace;
+
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
-  window["gtag"]("config", GA_TRACKING_ID, {
+  (window as any).gTag("config", GA_TRACKING_ID, {
     page_path: url,
   });
 };
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }) => {
-  window["gtag"]("event", action, {
+  (window as any).gTag("event", action, {
     event_category: category,
     event_label: label,
     value: value,
