b1e9c4b53db0ccd85697d5dc7ba2bd1bcf121412
Andre <andre.devries@theinformationlab.co.uk>
2022-01-21 17:47:12 +0000

    add www to tracking

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,13 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-import { GA_TRACKING_ID } from "@/lib/gtag";
+// import { GA_TRACKING_ID } from "@/lib/gtag";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
+          {/* <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           />
@@ -23,7 +22,7 @@ export default class MyDocument extends Document {
             });
           `,
             }}
-          />
+          /> */}
         </Head>
         <body>
           <Main />
