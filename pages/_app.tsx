ae8193be03faf5e3def454518321077e5fc5b3c9
Andre de Vries <aedevries19@gmail.com>
2023-06-02 15:58:23 +0100

    Add posthog

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,6 +18,21 @@ import CommandBar from "@/components/CommandBar";
 import { Analytics } from "@vercel/analytics/react";
 import Background from "@/components/background";
 
+// Posthog
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+// Check that PostHog is client-side (used to handle Next.js SSR)
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
+    // Enable debug mode in development
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug();
+    },
+  });
+}
+
 const meta = {
   title: "Andre de Vries",
   twitterCard: "",
@@ -35,7 +50,18 @@ const meta = {
 };
 
 function App({ Component, pageProps }) {
-  // const router = useRouter();
+  const router = useRouter();
+
+  useEffect(() => {
+    // Track page views
+    const handleRouteChange = () => posthog?.capture("$pageview");
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
   // useEffect(() => {
   //   const handleRouteChange = (url) => {
   //     gtag.pageview(url);
@@ -47,56 +73,58 @@ function App({ Component, pageProps }) {
   // }, [router.events]);
 
   return (
-    <CommandBar>
-      <div className="flex min-h-screen flex-col">
-        <div className="antialiased z-10">
-          <Head>
-            <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-            <link
-              rel="apple-touch-icon"
-              sizes="180x180"
-              href="/apple-touch-icon.png"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              sizes="32x32"
-              href="/favicon-32x32.png"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              sizes="16x16"
-              href="/favicon-16x16.png"
-            />
-            <link rel="manifest" href="/site.webmanifest" />
-            <meta name="theme-color" content="#ffffff" />
-            <meta content={meta.description} name="description" />
-            <meta content={meta.keywords.join(", ")} name="keywords" />
-            <meta property="og:type" content="website" />
-            <meta property="og:site_name" content="Andre de Vries" />
-            <meta property="og:description" content={meta.description} />
-            <meta property="og:title" content={meta.title} />
-            <title>{meta.title}</title>
-          </Head>
-          <Section>
-            <Header />
-          </Section>
-          <Section>
-            <Fragment>
-              <div className="py-28">
-                <Component {...pageProps} />
-              </div>
-            </Fragment>
-          </Section>
-          <Section>
-            <Footer />
-          </Section>
+    <PostHogProvider client={posthog}>
+      <CommandBar>
+        <div className="flex min-h-screen flex-col">
+          <div className="antialiased z-10">
+            <Head>
+              <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+              <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/apple-touch-icon.png"
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/favicon-32x32.png"
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/favicon-16x16.png"
+              />
+              <link rel="manifest" href="/site.webmanifest" />
+              <meta name="theme-color" content="#ffffff" />
+              <meta content={meta.description} name="description" />
+              <meta content={meta.keywords.join(", ")} name="keywords" />
+              <meta property="og:type" content="website" />
+              <meta property="og:site_name" content="Andre de Vries" />
+              <meta property="og:description" content={meta.description} />
+              <meta property="og:title" content={meta.title} />
+              <title>{meta.title}</title>
+            </Head>
+            <Section>
+              <Header />
+            </Section>
+            <Section>
+              <Fragment>
+                <div className="py-28">
+                  <Component {...pageProps} />
+                </div>
+              </Fragment>
+            </Section>
+            <Section>
+              <Footer />
+            </Section>
+          </div>
+          <Background />
         </div>
-        <Background />
-      </div>
-      <Analytics />
-    </CommandBar>
+        <Analytics />
+      </CommandBar>
+    </PostHogProvider>
   );
 }
 
